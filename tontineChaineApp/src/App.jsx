1dce2708928504ddff4833e9f865deb976136d7b
Friedbocovo <friedbocovo03@gmail.com>
2026-05-01 22:31:20 +0200

    first commit

---
+++ tontineChaineApp/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <div className='flex justify-center items-center h-screen bg-gray-100'>
+      <h1 className='text-3xl font-bold text-gray-800 text-center'>
+        <span className='text-6xl'>Bienvenue</span> <br/> ALAPINI OLAYINKA TETEDE JEAN-ARTHUR RAYAN <br/> et <br/> Géronime_KOUKOUDOUI
+      </h1>
+    </div>
+  )
+}
+
+export default App
